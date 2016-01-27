48f5b97b81a5e5a544c5e08013fbe1d21c4af69c
Wussypants <jacob.molin@me.com>
2016-01-27 12:20:32 +0100

    Initial Commit

---
+++ testTVGuide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testTVGuide
+//
+//  Created by Jacob Molin Nielsen on 27/01/2016.
+//  Copyright © 2016 Wussypants. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
